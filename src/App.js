4436a92b9cb8fd3cd0bc75ad812193c8ca37169f
hassoswitch5 <157315012+hassoswitch5@users.noreply.github.com>
2024-08-27 21:17:57 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Hello, world!</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
